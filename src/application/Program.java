63956fb13126be482bbd42ae5a3c0349f61c8b72
Moab Omena <1moabomena@gmail.com>
2019-10-17 20:24:50 -0300

    First class: Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,16 @@
 package application;
 
+import boardgame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello word");
+		
+		Position pos = new Position(3, 5);
+		
+		
+		System.out.println(pos);
 
 	}
 
